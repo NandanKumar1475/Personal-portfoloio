9537280013b85968b8ed742ad983e945ce84525a
Nandan <nandankumar1475@gmail.com>
2024-07-04 17:07:59 +0530

    changes done in animated typing

--- script.js
+++ script.js
@@ -28,6 +28,6 @@ setInterval(() => {
 
 // typing animiation code 
 var typed = new Typed('#element', {
-    strings: ['<h4>Fronted Developer</h4>.', '&amp; <h4>Full Stack Developer</h4>.','<h4>MERN Developer </h4>','<h4>Video Editior</h4>'],
+    strings: ['<h4>Fronted Developer</h4>.', '&amp; <h4>MERN Developer</h4>.','<h4>Video Editior </h4>','<h4>Full Stack Developer</h4>'],
     typeSpeed: 50,
   });
